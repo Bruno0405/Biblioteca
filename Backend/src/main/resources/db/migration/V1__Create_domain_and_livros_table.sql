ca8e5d997aba8045c3943566253400f501010e8c
Michelli <michelli.lima99@gmail.com>
2026-03-22 19:14:28 -0300

    Ajustes arquivo V1 livros

--- Backend/src/main/resources/db/migration/V1__Create_domain_and_livros_table.sql
+++ Backend/src/main/resources/db/migration/V1__Create_domain_and_livros_table.sql
@@ -1,13 +1,14 @@
 CREATE DOMAIN dm_padrao VARCHAR (80);
 
 CREATE TABLE Livros (
-	id_livro SERIAL NOT NULL,
-	nome_livro dm_padrao NOT NULL,
-	isnb CHAR(13) UNIQUE,
- 	editora dm_padrao NOT NULL,
-	ano INT,
- 	sinopse TEXT,
- 	localizacao_fisica dm_padrao,
+    id_livro INT GENERATED ALWAYS AS IDENTITY,
+    nome_livro dm_padrao NOT NULL,
+    isnb CHAR(13) UNIQUE,
+    editora dm_padrao NOT NULL,
+    ano INT,
+    sinopse TEXT,
+    localizacao_fisica dm_padrao,
+    data_adicao DATE NOT NULL,
 
-	PRIMARY KEY (id_livro)
+    PRIMARY KEY (id_livro)
 );
